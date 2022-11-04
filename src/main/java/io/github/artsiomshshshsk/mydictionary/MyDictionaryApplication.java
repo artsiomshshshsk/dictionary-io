1348650cdab6042d925ba77e88adafa7d652fb20
Artsiom Shablinski <artsiomshablinskiy@gmail.com>
2022-11-04 22:52:42 +0100

    test

--- src/main/java/io/github/artsiomshshshsk/mydictionary/MyDictionaryApplication.java
+++ src/main/java/io/github/artsiomshshshsk/mydictionary/MyDictionaryApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyDictionaryApplication {
-
     public static void main(String[] args) {
-        System.out.println("Hello world!");
         SpringApplication.run(MyDictionaryApplication.class, args);
     }
-
 }
---
+++ src/main/java/io/github/artsiomshshshsk/mydictionary/controller/HomeController.java
@@ -0,0 +1,15 @@
+package io.github.artsiomshshshsk.mydictionary.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/home")
+    public String hello(){
+        return "Hello world!";
+    }
+}
